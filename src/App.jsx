c05b5e4a71a8267fe846f04d3d86a76d8eb72215
Danchipizzeria <danchipizzeria@gmail.com>
2025-05-13 01:50:17 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,3 @@
-export default function App() { return <h1 style={{ fontFamily: 'Comfortaa', color: 'rgb(141, 26, 20)' }}>Pizzeria Danchi App Completa</h1>; }
+export default function App() {
+  return <h1 style={{ fontFamily: 'Comfortaa', color: 'rgb(141, 26, 20)', padding: 20 }}>Pizzeria Danchi App</h1>;
+}
